c7b55d28be69ab8a1461b967425035cf3030c439
Geno Roupsky <geno@roupsky.name>
2016-10-13 14:59:53 +0300

    fix production build

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ var Vue = require('vue');
 var Events = require('./components/Events.vue');
 
 new Vue({
-    el: '.app',
-    components: {
-        'hf-events': Events,
-    },
+    el: 'hf-events',
+    render: function(h){
+        return h(Events);
+    }
 });
